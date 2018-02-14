a9a5b5e48b3d820c6a9b8c67670c28e0f75741c6
mrT23 <talrid@gmail.com>
2018-02-14 14:08:21 +0200

    pdist handle NaN

--- matlab/+dagnn/PDist.m
+++ matlab/+dagnn/PDist.m
@@ -30,7 +30,8 @@ classdef PDist < dagnn.ElementWise
         function outputs = forward(obj, inputs, params)
             outputs{1} = vl_nnpdist(inputs{1}, inputs{2}, obj.p, 'noRoot', obj.noRoot, 'epsilon', obj.epsilon, 'aggregate', obj.aggregate, 'clip',obj.clip,'clipValue',obj.clipValue, obj.opts{:}) ;
             n = obj.numAveraged ;
-            m = n + size(inputs{1},4) ;
+            numOfNewOutputs=gather(sum(~isnan(sum(squeeze(inputs{1}-inputs{2}),1))));
+            m = n +numOfNewOutputs ;
             obj.average = (n * obj.average + gather(outputs{1})) / m ;
             obj.numAveraged = m ;
         end
